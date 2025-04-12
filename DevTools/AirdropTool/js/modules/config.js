47a4521a6677b511497056d23022d685e5839c2a
LQXTOKEN <digital.station.gr@gmail.com>
2025-04-12 11:15:38 +0300

    Create config.js

---
+++ DevTools/AirdropTool/js/modules/config.js
@@ -0,0 +1,10 @@
+// js/modules/config.js
+
+export const LQX_ADDRESS = "0x9e27f48659b1005b1abc0f58465137e531430d4b";
+export const LQX_REQUIRED = "1000";
+export const LQX_ABI = [
+  "function balanceOf(address account) external view returns (uint256)"
+];
+
+export const PROXY_URL = "https://proxy-git-main-lqxtokens-projects.vercel.app";
+export const MAIN_WEBSITE_URL = "https://liquidityx.io";
