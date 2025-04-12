01b47dd7685fe2e2d2d97a88ccd4c2281b12beb2
LQXTOKEN <digital.station.gr@gmail.com>
2025-04-12 19:38:03 +0300

    Update config.js

--- DevTools/AirdropTool/js/modules/config.js
+++ DevTools/AirdropTool/js/modules/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 
-export const CONFIG = {
+window.CONFIG = {
   POLYGON_RPC: "https://polygon-rpc.com",
   LQX_TOKEN_ADDRESS: "0x3cC5BfF1eB267bD0A0eAb358F8F1A5f3905C8794",
   STAKING_CONTRACT: "0x2012508a1dbE6BE9c1B666eBD86431b326ef6EF6",
