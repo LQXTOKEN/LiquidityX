8ccd56dd826f3d4fd00b7eba8f3fad9bb6b0bea3
LQXTOKEN <digital.station.gr@gmail.com>
2025-04-12 10:14:00 +0300

    Delete DevTools/AirdropTool/modules directory

--- DevTools/AirdropTool/modules/config.js
+++
@@ -1,9 +0,0 @@
-export const LQX_ADDRESS = "0x9e27f48659b1005b1abc0f58465137e531430d4b";
-
-export const LQX_ABI = [
-  "function balanceOf(address account) external view returns (uint256)"
-];
-
-export const LQX_REQUIRED = ethers.utils.parseUnits("1000", 18);
-
-export const PROXY_URL = "https://proxy-git-main-lqxtokens-projects.vercel.app";
