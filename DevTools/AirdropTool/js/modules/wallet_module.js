1701af3f3e8ed22fca6886bc3cb24afb3a5dd57b
LQXTOKEN <digital.station.gr@gmail.com>
2025-04-13 13:03:21 +0300

    Update wallet_module.js

--- DevTools/AirdropTool/js/modules/wallet_module.js
+++ DevTools/AirdropTool/js/modules/wallet_module.js
@@ -1,48 +1,27 @@
-// js/modules/wallet_module.js
-
 window.walletModule = (function () {
-  let provider = null;
-  let signer = null;
-  let userAddress = null;
+  let provider;
+  let signer;
+  let userAddress;
 
   async function connectWallet() {
-    console.log("[walletModule] Trying to connect wallet...");
-
-    if (window.ethereum) {
-      try {
-        provider = new ethers.providers.Web3Provider(window.ethereum);
-        console.log("[walletModule] Provider created:", provider);
-
-        await provider.send("eth_requestAccounts", []);
-        signer = provider.getSigner();
-        userAddress = await signer.getAddress();
-
-        console.log("[walletModule] Connected:", userAddress);
-        return { provider, signer, userAddress };
-      } catch (error) {
-        console.error("[walletModule] User rejected connection or error occurred:", error);
+    try {
+      if (!window.ethereum) {
+        alert("MetaMask is not installed!");
         return null;
       }
-    } else {
-      alert("MetaMask is not installed.");
-      console.warn("[walletModule] No window.ethereum found");
+
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+
+      provider = new ethers.providers.Web3Provider(window.ethereum);
+      signer = provider.getSigner();
+      userAddress = await signer.getAddress();
+
+      console.log("[walletModule] Connected:", userAddress);
+      return { provider, signer, userAddress };
+    } catch (error) {
+      console.error("[walletModule] Connection error:", error);
       return null;
     }
-  }
-
-  function disconnectWallet() {
-    console.log("[walletModule] Disconnecting wallet");
-    provider = null;
-    signer = null;
-    userAddress = null;
-  }
-
-  function isWalletConnected() {
-    return !!userAddress;
-  }
-
-  function getSigner() {
-    return signer;
   }
 
   function getProvider() {
@@ -53,12 +32,16 @@ window.walletModule = (function () {
     return userAddress;
   }
 
+  function disconnectWallet() {
+    provider = null;
+    signer = null;
+    userAddress = null;
+  }
+
   return {
     connectWallet,
-    disconnectWallet,
-    isWalletConnected,
-    getSigner,
     getProvider,
-    getUserAddress
+    getUserAddress,
+    disconnectWallet
   };
 })();
